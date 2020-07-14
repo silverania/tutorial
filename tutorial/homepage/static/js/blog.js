a3776f1ff9c6a79bd63b3fe655c4018a2d29ce38
silverania <spaceissimo@gmail.com>
2020-07-14 15:08:52 +0200

    click on button blog exec javascript to create textarea blog

--- tutorial/homepage/static/js/blog.js
+++ tutorial/homepage/static/js/blog.js
@@ -1,5 +1,13 @@
-function writeMsg(login){
-  if (login=="True"){
-    console.log("i'm logged");
-}
+
+function writeMsg(id,login){
+    var id=id;
+    class Blog{
+      constructor(){
+        alert(id);
+        parent=document.getElementById(id);
+        var area = document.createElement("TEXTAREA");
+        parent.appendChild(area);
+    }
+    }
+    new Blog();
 }
