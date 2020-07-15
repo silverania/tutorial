3e2af59608d86f7c263fe74350da4251f4832a2e
silverania <spaceissimo@gmail.com>
2020-07-15 17:47:08 +0200

    textarea commenti dimensionata e disabilitata dopo il primo click all interno .....per evitare che il client apra decine di textatarea inutili

--- tutorial/homepage/static/js/blog.js
+++ tutorial/homepage/static/js/blog.js
@@ -1,13 +1,28 @@
-
+function addTextArea(){
+  let title=document.getElementsByClassName("blog_title");
+  padre=document.getElementById("multiarea");
+  area=document.createElement("TEXTAREA");
+  area.setAttribute("rows","12");
+  padre.prepend(area);
+  $(area).animate({'width':'100%','margin-left':'-200px'},2000);
+  return 0;
+}
+function disableButtonComment(element){
+  element.disabled=true;
+}
 function writeMsg(id,login){
-    var id=id;
+  var figlio=document.getElementById(id);
+  var id=id;
+  var el;
+  var padre;
+  var area;
     class Blog{
       constructor(){
-        alert(id);
-        parent=document.getElementById(id);
-        var area = document.createElement("TEXTAREA");
-        parent.appendChild(area);
+        if(addTextArea(id)==0){
+          disableButtonComment(figlio);
+        }
     }
-    }
+  }
     new Blog();
+
 }
