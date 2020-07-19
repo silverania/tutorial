1c06f5b778690fb976bc0b18564978b4cf463b95
silverania <spaceissimo@gmail.com>
2020-07-19 12:35:05 +0200

    implementati model , e admin.py

--- tutorial/blog/admin.py
+++ tutorial/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from blog.models import Comment
 # Register your models here.
+@admin.register(Comment)
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ('title','body')
+    list_display = ('title','status','slug', 'created', 'publish', 'author')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
