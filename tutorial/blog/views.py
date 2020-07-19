50cb91a895a887ef00690587b06a40958fd81949
silverania <spaceissimo@gmail.com>
2020-07-19 17:10:26 +0200

    catturato usename in view

--- tutorial/blog/views.py
+++ tutorial/blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse;
 # Create your views here.
 def newPost(request):
+    # prendi l user collegato
     if request.GET['q']:
-        print("read q")
+        message=request.GET['q']
+        print("yes!")
+        username = None
+        if request.user.is_authenticated:
+            username = request.user.username
+            return HttpResponse("o yes , user logged is :"+username +" and messages is :"+message)
