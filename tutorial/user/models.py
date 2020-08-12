38c30cf43b60a25a65a3002ff290e3e168723b33
silverania <spaceissimo@gmail.com>
2020-08-12 14:34:49 +0200

    l user di django è mappato al custom user attraverso una relazione onetoonefield

--- tutorial/user/models.py
+++ tutorial/user/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
